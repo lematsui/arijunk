6b1049d4e98c65fb7794a7d9cd6cc7b3fece0bd4
Ariel LeRoy <arielmatsuileroy@gmail.com>
2019-01-03 10:11:42 +0000

    half finished meme#new view

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -1,6 +1,23 @@
 class MemesController < ApplicationController
-	skip_before_action :authenticate_user!, only: :index
+	skip_before_action :authenticate_user!, only: [:index, :show]
 
 	def index
+	end
+
+	def show
+	end
+
+	def new
+		@meme = Meme.new
+	end
+
+	def create
+		@meme = Meme.new(meme_params)
+	end
+
+	private
+
+	def meme_params
+  	params.require(:meme).permit(:photo, :caption, :tags, :title)
 	end
 end
